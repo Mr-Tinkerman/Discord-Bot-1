d4b856135c0d339643772286a2576c3ba813014b
Mr-Tinkerman <37818934+Mr-Tinkerman@users.noreply.github.com>
2018-04-19 19:05:12 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
-const prefix = config.prefix;
+const config_file = require("./config.json");
+var prefix = config_file.prefix;
 
 client.on("ready", () => {
   console.log("I am ready!");
