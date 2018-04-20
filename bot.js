a31f06c33c0d768b45bcdacdc51aea9baab9c7f1
Mr-Tinkerman <37818934+Mr-Tinkerman@users.noreply.github.com>
2018-04-19 20:44:37 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,21 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("config.json");
+const prefix = "!";
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix)) return;
+  if (!message.content.startsWith(prefix)) return;
 
-  if (message.content.startsWith(config.prefix + "ping")) {
+  if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong");
   } else
-  if (message.content.startsWith(config.prefix + "Hi") || message.content.startsWith(config.prefix + "Hello") || message.content.startsWith(config.prefix + "Herro")) {
+  if (message.content.startsWith(prefix + "Hi") || message.content.startsWith(prefix + "Hello") || message.content.startsWith(prefix + "Herro")) {
     message.channel.send("Hi... how are you?");
   }
 });
 
 client.login(process.env.BOT_TOKEN);
-//(config.prefix + "");
+//(prefix + "");
